b61fe1ac6ab047e472d274e6599630a1239c5508
Mamadou Maladho Diallo <diallomamadoumaladho62102@gmail.com>
2026-04-30 03:59:43 -0400

    Fix babel config for production build

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,17 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const isDev = process.env.NODE_ENV === "development";
+
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "react-refresh/babel",
+      isDev && "react-refresh/babel",
       [
         "module-resolver",
         {
           root: ["./"],
           alias: { "@": "./src" },
-          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
-        }
-      ]
-    ]
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        },
+      ],
+    ].filter(Boolean),
   };
 };
